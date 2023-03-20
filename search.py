1cb0214f8b18f027f92a8a9ce0e1495d6c885d4b
ramon.leal <ramon.leal@agu.gov.br>
2023-03-20 17:01:04 -0300

    Verscao-01

--- search.py
+++ search.py
@@ -8,7 +8,6 @@ class search_google():
     headers = {
       'Cookie': 'NID=511=knOEsEv229h9dcJiCeCO5PSBcVClGSruFnVl-Ak3ppkDAiyET0YngRAY9rNuqulLbXyHHnKVtaZcBgXA2rEjDAPIAsXuNZUgqaBf6e6hEKgVQcl2HnszEAJjw0g6--oTR5Suk0R719pigX8r8UIL71umyh3BoI_Zq6cLoBpQOus'
     }
-
     response = requests.request("GET", url, headers=headers, data=payload, files=files)
 
     print(response.text)
